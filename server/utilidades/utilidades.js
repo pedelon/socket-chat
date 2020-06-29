b91575b10cddcdd0fedd244bf7e6c7ec7160e3fe
PedroD <pedelon@gmail.com>
2020-06-29 12:01:41 -0400

    first commit

---
+++ server/utilidades/utilidades.js
@@ -0,0 +1,13 @@
+const crearMensaje = (nombre, mensaje) => {
+
+    return {
+        nombre,
+        mensaje,
+        fehca: new Date().getTime()
+    };
+
+}
+
+module.exports = {
+    crearMensaje
+}
